08e1502e8cd4dc29169a94221d855f6cf426b99a
Olivier Ligot <oligot@gmail.com>
2015-11-25 13:28:47 +0100

    Use the new optional parameter to transform the path

---
+++ hot-reload.js
@@ -0,0 +1,17 @@
+(function() {
+  var readyForMainLoad;
+  if (location.origin.match(/localhost/)) {
+    System.trace = true;
+    readyForMainLoad = System.import('jspm-hot-reloader').then(function(HotReloader) {
+      var url = 'http://' + document.location.hostname + ':' + document.location.port;
+      hr = new HotReloader.default(url, function (path) {
+        return path.replace(document.location.pathname.slice(1), '');
+      });
+    });
+  }
+  Promise.resolve(readyForMainLoad).then(() => {
+    System.import('app').then(function () {
+      console.log('ran at ', new Date());
+    });
+  });
+})();
