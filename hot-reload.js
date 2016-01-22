cc7ac1a6f19148ef89d512777eb34afed36dddb2
Olivier Ligot <oligot@gmail.com>
2016-01-22 11:37:19 +0100

    Module has been renamed from jspm-hot-reloader to systemjs-hot-reloader

--- hot-reload.js
+++ hot-reload.js
@@ -2,7 +2,7 @@
   var readyForMainLoad;
   if (location.origin.match(/localhost/)) {
     System.trace = true;
-    readyForMainLoad = System.import('jspm-hot-reloader').then(function(HotReloader) {
+    readyForMainLoad = System.import('systemjs-hot-reloader').then(function(HotReloader) {
       var url = 'http://' + document.location.hostname + ':' + document.location.port;
       hr = new HotReloader.default(url, function (path) {
         return path.replace(document.location.pathname.slice(1), '');
